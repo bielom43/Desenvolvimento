2637381c5b926c405ce0d39fca2a5928ed48be93
bielom43 <bielom43@gmail.com>
2022-09-13 16:29:43 -0300

    updating

--- Dart/Funcoes/7_funcao_arrow.dart
+++ Dart/Funcoes/7_funcao_arrow.dart
@@ -1,11 +1,12 @@
 void main(List<String> args) {
-  var multiplicacao = (int a, int b) {
-    return a * b;
-  };
-
-  print(multiplicacao(23, 2));
+  var multiplicacao = (int a, int b) => {a * b};
 
   var adicao = (int a, int b) => /*arrow*/ a + b;
+  var divisao = (int a, int b) => /*arrow*/ a / b;
+  var subtracao = (int a, int b) => /*arrow*/ a - b;
 
+  print(multiplicacao(23, 2));
   print(adicao(22, 54));
+  print(subtracao(22, 54));
+  print(divisao(22, 54));
 }
